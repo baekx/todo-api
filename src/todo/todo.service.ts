96189830e0d1ae540d347340f4039cbb85f60e40
seo <seungbaek.seo@spacemarket.co.jp>
2023-05-26 18:21:40 +0900

    dummy data作成

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,32 +1,28 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Todo, TodoStatus } from './models/todo.models';
 import { ID } from '@nestjs/graphql';
+import { todos } from './models/dummy';
 
 @Injectable()
 export class TodoService {
 
-    static todo1: Todo = {
-        id: '1',
-        title: 'todo1',
-        description: null,
-        status: TodoStatus.NEW,
-        createdAt: null,
-        updatedAt: null
-    };
 
-    private todos: Todo[] = [
-        TodoService.todo1,
-        TodoService.todo1,
-        TodoService.todo1,
-        TodoService.todo1,
-    ];
+    // static todo1: Todo = {
+    //     id: '1',
+    //     title: 'todo1',
+    //     description: null,
+    //     status: TodoStatus.NEW,
+    //     createdAt: null,
+    //     updatedAt: null
+    // };
+
 
     findAll(): Todo[] {
-        return this.todos;
+        return todos;
     }
 
     findOneById(id: string): Todo {
-        const result = this.todos.find((todo) =>
+        const result = todos.find((todo) =>
             id === todo.id
         );
         if (!result) {
